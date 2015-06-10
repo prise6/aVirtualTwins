f00b8835ee468f300183aaacaa181d599cb0fc8a
prise6 <vieille.francois@gmail.com>
2015-06-10 15:24:53 +0200

    Difft documentation

--- R/difft.R
+++ R/difft.R
@@ -1,12 +1,23 @@
 
-#' A reference class to represent difference between twin1 and twin2
-#'
-#' @include object.R
+#' Difference between twins
 #' 
+#' A reference class to represent difference between twin1 and twin2
+#' 
+#' Difft are calculated depending on the favorable outcome chosen. It is the
+#' second level of the outcome. For example, if the outcome is 0 and 1, the
+#' favorable outcome is 1. Then, \deqn{difft_i = twin1_i - twin2_i IF T_i =
+#' 1} \deqn{ difft_i = twin2_i - twin1_i IF T_i = 0}
+#' 
+#' @include object.R
+#'   
 #' @field vt.object VT.object (refClass) representing data
-#' @field twin1 vector of \eqn{E(Y|T= real treatment)}
-#' @field twin2 vector of \eqn{E(Y|T= antoher treatment)}
-#' @field difft vector of difference between twin1 and twin2 
+#' @field twin1 vector of \eqn{E(Y|T = real treatment)}
+#' @field twin2 vector of \eqn{E(Y|T = anoher treatment)}
+#' @field difft vector of difference between twin1 and twin2
+#'   
+#' @name VT.difft
+#'  
+#' @seealso \code{\link{VT.forest}}, \code{\link{VT.forest.one}}, \code{\link{VT.forest.double}}
 #' 
 #' @import methods
 VT.difft <- setRefClass(
@@ -30,7 +41,7 @@ VT.difft <- setRefClass(
     },
     
     computeDifft = function(){
-      "Compute difference between twin1 and twin2"
+      "Compute difference between twin1 and twin2. See details."
       
       if(sum(is.na(.self$twin1)) != 0 | sum(is.na(.self$twin2)) != 0 ) stop("Twins must be valid")
       
